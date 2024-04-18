66f5b8e9d097e00f7876fddb4d518f7fab7c6349
Farhan Jafri <farhan@joinzorro.com>
2024-04-18 06:56:37 +0530

    make gethello public

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Public } from './decorators';
 
 @Injectable()
 export class AppService {
-  @Public()
   getHello(): string {
     return 'Hello World!';
   }
